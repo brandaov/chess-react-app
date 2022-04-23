f3c91c7018b0d9c36c0f7c964c2d9b4b0a622b09
Victor <victor.brandao@itlab.com.br>
2022-04-23 12:54:16 -0300

    Adicionando regras rainha

---
+++ src/arbitro/regras/Rainha.ts
@@ -0,0 +1,23 @@
+import { Peca, Posicao, mesmaPosicao, TipoTime } from "../../Constants";
+import { casaVaziaOuOcupadaPeloOponente, casaOcupada } from "./RegrasGerais";
+
+export const movimentoRainha = (posicaoInicial: Posicao, posicaoDesejada: Posicao, time: TipoTime, tabuleiroState: Peca[]): boolean => {
+    for(let i = 1; i < 8; i++) {
+      //Diagonal
+      let multiplicadorX = (posicaoDesejada.x < posicaoInicial.x) ? -1 : (posicaoDesejada.x > posicaoInicial.x) ? 1 : 0;
+      let multiplicadorY = (posicaoDesejada.y < posicaoInicial.y) ? -1 : (posicaoDesejada.y > posicaoInicial.y) ? 1 : 0;
+
+      let posicaoPassada: Posicao = {x: posicaoInicial.x + (i * multiplicadorX), y: posicaoInicial.y + (i * multiplicadorY)};
+
+      if(mesmaPosicao(posicaoPassada, posicaoDesejada)) {
+        if(casaVaziaOuOcupadaPeloOponente(posicaoPassada, tabuleiroState, time)) {
+          return true;
+        }
+      } else {
+        if(casaOcupada(posicaoPassada, tabuleiroState)) {
+          break;
+        }
+      }
+    }
+    return false;
+  }
