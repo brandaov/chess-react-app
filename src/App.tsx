3065b343c7f8ce1629ab1d0cfa7962ec6bb9646f
Victor <victor.brandao@itlab.com.br>
2022-04-23 12:46:00 -0300

    criando projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Tabuleiro from './components/Tabuleiro/Tabuleiro';
+
+function App() {
+  return (
+    <div id="app">
+      <Tabuleiro/>
+    </div>
+  );
+}
+
+export default App;
