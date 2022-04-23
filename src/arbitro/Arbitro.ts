21c04f7e7e2c47db14a420339515510541a2ae35
Victor <victor.brandao@itlab.com.br>
2022-04-23 12:52:59 -0300

    adicionando check de validação dos movimentos

---
+++ src/arbitro/Arbitro.ts
@@ -0,0 +1,79 @@
+import {
+  TipoPeca,
+  TipoTime,
+  Peca,
+  Posicao,
+} from "../Constants";
+
+import {
+  movimentoPeao,
+  movimentoCavalo,
+  movimentoBispo,
+  movimentoTorre,
+  movimentoRainha,
+  movimentoRei,
+} from "./regras";
+
+export default class Arbitro {
+  enPassant(
+    posicaoInicial: Posicao,
+    posicaoDesejada: Posicao,
+    tipo: TipoPeca,
+    time: TipoTime,
+    tabuleiroState: Peca[]
+  ) {
+    const direcaoPeao = time === TipoTime.JOGADOR ? 1 : -1;
+
+    if (tipo === TipoPeca.PEAO) {
+      if (
+        (posicaoDesejada.x - posicaoInicial.x === -1 ||
+          posicaoDesejada.x - posicaoInicial.x === 1) &&
+        posicaoDesejada.y - posicaoInicial.y === direcaoPeao
+      ) {
+        const peca = tabuleiroState.find(
+          (p) =>
+            p.posicao.x === posicaoDesejada.x &&
+            p.posicao.y === posicaoDesejada.y - direcaoPeao &&
+            p.enPassant
+        );
+        if (peca) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  // TODO:
+  // Promoção de peões
+  // Prevenir que o rei se mova para uma casa em que possa ser consumido
+  // Castling
+  // Check
+  // Checkmate
+  // Stalemate
+  isMovimentoValido(posicaoInicial: Posicao, posicaoDesejada: Posicao, tipo: TipoPeca, time: TipoTime, tabuleiroState: Peca[]) {
+    let movimentoValido = false;
+    switch (tipo) {
+      case TipoPeca.PEAO:
+        movimentoValido = movimentoPeao(posicaoInicial, posicaoDesejada, time, tabuleiroState);
+        break;
+      case TipoPeca.TORRE:
+        movimentoValido = movimentoTorre(posicaoInicial, posicaoDesejada, time, tabuleiroState);
+        break;
+      case TipoPeca.BISPO:
+        movimentoValido = movimentoBispo(posicaoInicial, posicaoDesejada, time, tabuleiroState);
+        break;
+      case TipoPeca.CAVALO:
+        movimentoValido = movimentoCavalo(posicaoInicial, posicaoDesejada, time, tabuleiroState);
+        break;
+      case TipoPeca.RAINHA:
+        movimentoValido = movimentoRainha(posicaoInicial, posicaoDesejada, time, tabuleiroState);
+        break;
+      case TipoPeca.REI:
+        movimentoValido = movimentoRei(posicaoInicial, posicaoDesejada, time, tabuleiroState);
+    }
+
+    return movimentoValido;
+  }
+}
